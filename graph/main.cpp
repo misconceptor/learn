a26130af238c34c332d3fba80902ca1a2bb02598
misconceptor <thespeedcubers347@gmail.com>
2025-03-01 06:49:29 +0300

    graph-new

--- graph/main.cpp
+++ graph/main.cpp
@@ -4,8 +4,7 @@ const int MAX_NODES = 100;
 
 struct Node{
     int numIn,numOut;
-    Node() : numIn(0), numOut(0) {
-    }
+    Node() : numIn(0), numOut(0) {}
 
     void incIn(){
         ++numIn;
@@ -28,38 +27,53 @@ struct Edge
         return in;
     }
     friend ostream& operator << (ostream& os, Edge const& edge){ 
-        return os << edge.inIdx << ' ' << edge.outIdx << endl;
+        return os << edge.inIdx << ' ' << edge.outIdx;// << endl;
     }
     bool isLoop(){
         return inIdx == outIdx;
     }
 };
 
-
 int main(){
-    //ifstream input("in.txt");
-    /*if(input){
-        copy(istream_iterator<Edge>(input), 
-             istream_iterator<Edge>(), back_inserter(a));
-        for(auto i : a){
-            cout << i << endl;
-        }
-    }*/
-
-
-    //сам нод даже не хранит свое значение, тк это его номер в массиве
-    //TODO сделай перебор ребер и подсчет inOut из каждой ноды которая !isLoop()
+    map<pair<int, int>, int> m;
+    vector<Edge> edges, e1;
     int n;
-    cin >> n;
-    Edge edges[n];
-    for(int i=0;i<n;++i){
-        cin >> edges[i];
+    ifstream input("in.txt");
+
+    if(!input) {
+        cout << "error\n";
+        return 0;
     }
+    //PROBLEM: DOUBLE CIN FOR THE LAST EDGE
+    /*for(int i=0;i<2;++i){
+        Edge e;
+        cin >> e;
+        edges.push_back(e);
+    }*/
+    input >> n;
     for(int i=0;i<n;++i){
-        cout << edges[i];
+        Edge t;
+        input >> t;
+        e1.push_back(t); 
     }
-    //todo print what you need
-    /*TODO: у тебя есть класс ребер из классов вершин. Надо найти сколько ребер
-    исходит из вершин каждого ребра.*/ 
+    copy(e1.begin(), e1.end(), back_inserter(edges));
+    /*cout << "size=" << edges.size() << endl;
+    for(int i=0;i<edges.size();++i){
+        cout << "(" << edges[i] << "):";
+        if(edges[i].isLoop()) cout << "loop ";
+        cout << nodes[edges[i].inIdx].numIn <<' '<< nodes[edges[i].inIdx].numOut <<"   ";
+        cout << nodes[edges[i].outIdx].numIn <<' '<< nodes[edges[i].outIdx].numOut << endl;
+    }
+    return 0;*/
+    for(int i=0;i<edges.size();++i){
+        pair<int,int> curEdge = make_pair(edges[i].inIdx, edges[i].outIdx);
+        m[curEdge] = nodes[edges[i].inIdx].numIn + nodes[edges[i].inIdx].numOut;
+        if(!edges[i].isLoop()){
+            m[curEdge] += nodes[edges[i].outIdx].numIn + nodes[edges[i].outIdx].numOut;
+        }
+    }
+    for(auto i = m.begin(); i != m.end(); ++i){
+        cout << "("<<i->first.first << '-' << i->first.second << "):  " << i->second << endl;
+    }
     return 0;
 }
